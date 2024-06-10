3004dca7ea6aa892296182bfc6a3667b88749e30
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2024-06-11 05:05:24 +0530

    week 2.2

---
+++ Week 2/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log(req.body);
+    res.send('Cohort khatam karo!');
+})
+
+app.listen(port);
