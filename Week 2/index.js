9a63e580a74dc2a046252c99ab1511006235d376
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2024-06-19 22:24:03 +0530

    update body middleware

--- Week 2/index.js
+++ Week 2/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log(req.body);
