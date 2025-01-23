301942d29f5566029a2559db3b63d9ae88df61eb
mdarslan7 <mdarslan7@gmail.com>
2025-01-23 18:49:52 +0530

    add /interest endpoint

--- Week 4/sum-server.js
+++ Week 4/sum-server.js
@@ -12,4 +12,16 @@ app.get("/sum", (req, res) => {
   res.send(sum.toString());
 });
 
+app.get("/interest", (req, res) => {
+    const principal = parseInt(req.query.p);
+    const rate = parseInt(req.query.r);
+    const time = parseInt(req.query.t);
+    const interest = (principal * rate * time) / 100;
+    const amount = principal + interest;
+    res.send({
+        interest: interest,
+        amount: amount
+    })
+}); 
+
 app.listen(3000);
